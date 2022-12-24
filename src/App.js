dfc1a256a761b26509bf721031417db6f671d689
Yurii Rybachok <drummer2009@ukr.net>
2022-12-24 18:37:46 +0200

    form for create post

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
   ])
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <button>Создать пост</button>
+      </form>
       <PostList posts={posts} title="Посты про JS"/>
     </div>
   );
