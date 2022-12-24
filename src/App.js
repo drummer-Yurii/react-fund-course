d308be337431f8e3fc5ab009d4aa46754016d683
Yurii Rybachok <drummer2009@ukr.net>
2022-12-24 19:11:20 +0200

    add data from input

--- src/App.js
+++ src/App.js
@@ -10,12 +10,25 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'description'},
     {id: 3, title: 'JavaScript 3', body: 'description'}
   ])
+
+  const [title, setTitle] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="Название поста" />
+        <MyInput
+          value={title} 
+          onChange={e => setTitle(e.target.value)}
+          type="text" 
+          placeholder="Название поста" 
+        />
         <MyInput type="text" placeholder="Описание поста" />
-        <MyButton>Создать пост</MyButton>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title="Посты про JS"/>
     </div>
