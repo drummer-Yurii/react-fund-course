7abbe2b9d5b7f1ef071bfbfe6ef8d0521f869a6a
Yurii Rybachok <drummer2009@ukr.net>
2022-12-24 18:33:55 +0200

    work with arr posts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import PostList from "./components/PostList";
+import './styles/App.css';
 
-function App() {
-  const [value, setValue] = useState('Текст в инпуте')
-  
-  
+function App() {  
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'description'},
+    {id: 2, title: 'JavaScript 2', body: 'description'},
+    {id: 3, title: 'JavaScript 3', body: 'description'}
+  ])
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <ClassCounter />
+      <PostList posts={posts} title="Посты про JS"/>
     </div>
   );
 }
