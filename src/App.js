797f83492fee041ae0907790f801a74e69a0f586
Yurii Rybachok <drummer2009@ukr.net>
2022-12-25 10:10:58 +0200

    selected sort of posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from "react";
+import React, {useState } from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css';
 
 function App() {
@@ -12,6 +11,8 @@ function App() {
     { id: 3, title: 'JavaScript 3', body: 'description' }
   ]) 
 
+  const [selectedSort, setSelectedSort] = useState('')
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -20,9 +21,26 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+      setSelectedSort(sort)
+      setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin: '15px 0'}} />
+      <div>
+       <MySelect
+          value={selectedSort}
+          onChange={sortPosts} 
+          defaultValue="Сортировка"
+          option={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+       />
+      </div>
       {posts.length !== 0
         ? <PostList remove={removePost} posts={posts} title="Посты про JS" />
         : <h1 style={{textAlign: 'center'}}>
