55b301650a03ed035a8036c917c850fcf0fcde42
Yurii Rybachok <drummer2009@ukr.net>
2022-12-25 12:34:33 +0200

    create hook and refactoring code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React, {useMemo, useState } from "react";
+import React, { useState } from "react";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'description' },
-    { id: 2, title: 'JavaScript 2', body: 'description' },
-    { id: 3, title: 'JavaScript 3', body: 'description' }
-  ]) 
-
- const [filter, setFilter] = useState({sort: '', query: ''})
- const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-  }
-      return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-      return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const [posts, setPosts] = useState([])
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -38,18 +24,18 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Создать пост
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-      <hr style={{margin: '15px 0'}} />
-      <PostFilter 
-        filter={filter} 
-        setFilter={setFilter} 
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
       />
-       <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />      
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
     </div>
   );
 }
