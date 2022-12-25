7e66f488a6752756ebd4452a43c0bca1df952cf2
Yurii Rybachok <drummer2009@ukr.net>
2022-12-25 13:04:28 +0200

    create api postService

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import PostService from "./API/PostService";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
@@ -24,8 +25,8 @@ function App() {
   }
 
   async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
+    const posts = await PostService.getAll()
+    setPosts(posts)
   }
 
   const removePost = (post) => {
