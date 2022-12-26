e9e300745c23c00d6e361a57159df895c3ed8fc4
Yurii Rybachok <drummer2009@ukr.net>
2022-12-26 21:21:25 +0200

    page login and routing for him

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import About from '../pages/About';
-import Posts from '../pages/Posts';
-import Error from '../pages/Error';
-import PostIdPage from '../pages/PostIdPage';
-import { routes } from '../router';
+import {publicRoutes, privateRoutes} from '../router';
 
 const AppRouter = () => {
+  const isAuth = false;
   return (
-    <Switch>
-      {routes.map(route => 
+    isAuth
+      ? 
+      <Switch>
+      {privateRoutes.map(route => 
         <Route 
           component={route.component} 
           path={route.path} 
@@ -18,6 +17,18 @@ const AppRouter = () => {
       )}
       <Redirect to='/posts' />
     </Switch>
+      :
+      <Switch>
+      {publicRoutes.map(route => 
+        <Route 
+          component={route.component} 
+          path={route.path} 
+          exact={route.exact} 
+        />
+      )}
+      <Redirect to='/login' />
+    </Switch>
+    
   );
 };
 
